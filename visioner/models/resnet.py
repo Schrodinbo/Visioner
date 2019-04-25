ed09da3daac9ca537a4bdc991cbcc48fd2095260
Schrodinbo <schrodinbo@gmail.com>
2019-04-25 15:09:30 -0600

    Just in case

--- visioner/models/resnet.py
+++ visioner/models/resnet.py
@@ -1,105 +1,101 @@
-import torch
-from torch import nn
-
 import torch.nn as nn
 import torchvision.models as models
-from visioner.models.layers import AdaptiveConcatPool2d, Flatten
+
+from torchvision.models.resnet import BasicBlock, Bottleneck
+
+from visioner.models.modules import LogitsHead
 
 __all__ = ['VisionResNet']
 
 resnets = {
-    18: models.resnet18,
-    34: models.resnet34,
-    50: models.resnet50,
-    101: models.resnet101,
-    152: models.resnet152
+    'resnet18': {
+        'model': models.resnet18,
+        'num_features': 512 * BasicBlock.expansion
+    },
+    'resnet34': {
+        'model': models.resnet34,
+        'num_features': 512 * BasicBlock.expansion
+    },
+    'resnet50': {
+        'model': models.resnet50,
+        'num_features': 512 * Bottleneck.expansion
+    },
+    'resnet101': {
+        'model': models.resnet101,
+        'num_features': 512 * Bottleneck.expansion
+    },
+    'resnet152': {
+        'model': models.resnet152,
+        'num_features': 512 * Bottleneck.expansion
+    },
 }
 
 
 class ResNetFeatureExtractor(nn.Module):
-    def __init__(self, version, pretrained=True, with_pooling=True, **kwargs):
+    def __init__(self, arch, pretrained=True, with_pooling=True, **kwargs):
         super(ResNetFeatureExtractor, self).__init__()
         self.with_pooling = with_pooling
-        assert version in resnets
-        self.net = resnets[version](pretrained=pretrained, **kwargs)
+        assert arch in resnets, 'Only ResNet18/34/50/101/152 are supported at this moment.'
+        self.resnet = resnets[arch]['model'](pretrained=pretrained, **kwargs)
 
     def forward(self, x):
-        x = self.net.conv1(x)
-        x = self.net.bn1(x)
-        x = self.net.relu(x)
+        x = self.resnet.conv1(x)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
         if self.with_pooling:
-            x = self.net.maxpool(x)
+            x = self.resnet.maxpool(x)
 
-        x = self.net.layer1(x)
-        x = self.net.layer2(x)
-        x = self.net.layer3(x)
-        x = self.net.layer4(x)
+        x = self.resnet.layer1(x)
+        x = self.resnet.layer2(x)
+        x = self.resnet.layer3(x)
+        x = self.resnet.layer4(x)
 
         return x
 
 
-class ClassificationHead(nn.Module):
-    default_pooling = nn.AdaptiveAvgPool2d(output_size=1)
-
-    def __init__(self, num_features=512, num_classes=10, global_pooling_mode='concat', bn_final=False, dropout=0.):
-        super(ClassificationHead, self).__init__()
-
-        layers = []
-        # global pooling
-        assert global_pooling_mode in ['avg', 'max', 'concat'], 'Invalid pooling type for building classification head.'
-        global_pooling = ClassificationHead.default_pooling
-        if global_pooling_mode == 'avg':
-            global_pooling = nn.AdaptiveAvgPool2d(output_size=1)
-        elif global_pooling_mode == 'max':
-            global_pooling = nn.AdaptiveMaxPool2d(output_size=1)
-        elif global_pooling_mode == 'concat':
-            global_pooling = AdaptiveConcatPool2d(output_size=1)
-        layers.append(global_pooling)
-        layers.append(Flatten())
-
-        if dropout > 0.:
-            layers.append(torch.nn.Dropout(p=dropout))
-
-        if bn_final:
-            layers.append(nn.BatchNorm1d(num_features, momentum=0.01))
-
-        layers.append(nn.Linear(num_features, num_classes))
-
-        self.head = nn.Sequential(*layers)
-
-    def forward(self, x):
-        return self.head(x)
-
-
 class VisionResNet(nn.Module):
-    def __init__(self, version, num_classes=10, pretrained=True, with_pooling=True, global_pooling_mode='avg',
-                 bn_final=False, dropout=0., mode='classification',
+    def __init__(self,
+                 arch,
+                 num_classes=10,
+                 pretrained=True,
+                 with_pooling=True,
+                 global_pooling_mode='avg',
+                 dropout=0.,
+                 mode='logits',
                  **kwargs):
         super(VisionResNet, self).__init__()
-        self.version = version
+        self.arch = arch
         self.pretrained = pretrained
         self.global_pooling_mode = global_pooling_mode
         self.with_pooling = with_pooling
         self.kwargs = kwargs
         self.num_classes = num_classes
         self.dropout = dropout
-        self.bn_final = bn_final
         self.mode = mode
         self.global_pooling = None
         self.body = self._create_body()
         self.head = self._create_head()
 
     def _create_body(self):
-        body = ResNetFeatureExtractor(self.version, pretrained=self.pretrained, with_pooling=self.with_pooling,
-                                      **self.kwargs)
+        body = ResNetFeatureExtractor(
+            self.arch,
+            pretrained=self.pretrained,
+            with_pooling=self.with_pooling,
+            **self.kwargs
+        )
         return body
 
     def _create_head(self):
         # TODO: support other vision tasks
-        if self.mode == 'classification':
-            head = ClassificationHead(num_features=512, num_classes=self.num_classes,
-                                      global_pooling_mode=self.global_pooling_mode, bn_final=self.bn_final,
-                                      dropout=self.dropout)
+        if self.mode == 'logits':
+            head = LogitsHead(
+                num_features=resnets[self.arch]['num_features'],
+                num_classes=self.num_classes,
+                global_pooling_mode=self.global_pooling_mode,
+                dropout=self.dropout
+            )
+        else:
+            raise NotImplementedError('Currently only the MLP head is supported.')
 
         return head
 
@@ -107,3 +103,22 @@ class VisionResNet(nn.Module):
         x = self.body(x)
         x = self.head(x)
         return x
+
+
+if __name__ == '__main__':
+    from visioner.models.test_resnet import check_resnet
+
+    model = VisionResNet('resnet18')
+    check_resnet(model)
+
+    model = VisionResNet('resnet34')
+    check_resnet(model)
+
+    model = VisionResNet('resnet50')
+    check_resnet(model)
+
+    model = VisionResNet('resnet101')
+    check_resnet(model)
+
+    model = VisionResNet('resnet152')
+    check_resnet(model)
